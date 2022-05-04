44b1dec718154e9e5263f3a8b804fb038bfb9089
Vikulov Andrey <vikulov.prog@gmail.com>
2022-05-04 22:28:05 +0400

    Initial commit

---
+++ config/init.php
@@ -0,0 +1,18 @@
+<?php
+
+define("DEBUG", 0);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT. '/app');
+define("CORE", ROOT . '/vendor/wfm');
+define("HELPERS", ROOT . '/vendor/wfm/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'ishop');
+define("PATH", 'http://vp-shop-2.local');
+define("ADMIN", 'http://vp-shop-2.local/admin');
+define("NO_IMAGE", 'uploads/no_image.jpg');
+
+
+require_once ROOT . '/vendor/autoload.php';
---
+++ public/errors/404.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Document</title>
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&family=Roboto:wght@100;300&display=swap");
+        .page_404{ padding:40px 0; background:#fff; font-family: 'Arvo', serif;
+        }
+
+        .page_404  img{ width:100%;}
+
+        .four_zero_four_bg{
+
+            background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+            height: 400px;
+            background-position: center;
+        }
+
+
+        .four_zero_four_bg h1{
+            font-size:80px;
+        }
+
+        .four_zero_four_bg h3{
+            font-size:80px;
+        }
+
+        .link_404{
+            color: #fff!important;
+            padding: 10px 20px;
+            background: #39ac31;
+            margin: 20px 0;
+            display: inline-block;}
+        .contant_box_404{ margin-top:-50px;}
+
+    </style>
+</head>
+<body>
+<section class="page_404">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 ">
+                <div class="col-sm-10 col-sm-offset-1  text-center">
+                    <div class="four_zero_four_bg">
+                        <h1 class="text-center ">404</h1>
+
+
+                    </div>
+
+                    <div class="contant_box_404">
+                        <h3 class="h2">
+                            Похоже вы не правильно набрали адрес!
+                        </h3>
+
+                        <p>Такой страницы не существует</p>
+
+                        <a href="<?=PATH?>" class="link_404">Перейти на главную</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
